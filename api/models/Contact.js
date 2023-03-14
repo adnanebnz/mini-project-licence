26393a3e0873c3839b35c980e7f6217857f2c6a9
skillz <skillzdev@hotmail.com>
2023-03-14 19:09:55 +0100

    fixed timestaps

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
-const contactSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const contactSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    msg: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  msg: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 module.exports = mongoose.model("Contact", contactSchema);
--- api/models/Item.js
+++ api/models/Item.js
@@ -1,44 +1,47 @@
 const mongoose = require("mongoose");
 
-const itemSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const itemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    img2: {
+      type: String,
+      required: true,
+    },
+    img3: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
   },
-  desc: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: String,
-    required: true,
-  },
-  img2: {
-    type: String,
-    required: true,
-  },
-  img3: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 5,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Item", itemSchema);
